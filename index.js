e1a591bda6edb57597435fb33920931f80f986ae
Your Name <usamax089@gmail.com>
2024-09-20 17:16:33 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution in this file!
+const employee = {
+    name: "effie",
+    adress: 254,
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    //create a copy to update destructively
+    const newObj = {...obj};
+
+    // assign a new value
+    newObj[key] = value;
+
+    return newObj;
+}
+
+const newO = updateEmployeeWithKeyAndValue(employee, "name2", "effiecancode");
+console.log(newO);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    // using [] notation
+    obj[key] = value;
+
+    return obj;
+}
+
+const newOb = destructivelyUpdateEmployeeWithKeyAndValue(employee, "adress2", "255");
+console.log(newOb);
+
+function deleteFromEmployeeByKey(obj, key) {
+    // create a copy
+    const newObje = {...obj};
+
+    // do delete
+    delete newObje[key];
+
+    // return a new updated obj
+    return newObje;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+
+    return obj;
+}
